88ec64119b1931a0791af6df9d6f3fbf807ed1b4
Ben M <ben@mosesweb.co.uk>
2023-05-14 14:35:24 +0100

    feature: add files for models, controllers and migrations for core application entities

---
+++ app/Http/Controllers/AdultController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Adult;
+use Illuminate\Http\Request;
+
+class AdultController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Adult $adult)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Adult $adult)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Adult $adult)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Adult $adult)
+    {
+        //
+    }
+}
